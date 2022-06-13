0e9037133a3c6d022e7b90d9a51cd01cf248eb7b
Frantxu1971 <106550711+Frantxu1971@users.noreply.github.com>
2022-06-13 20:00:53 +0200

    Actualizar pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,19 +1,16 @@
 function RotIzq (Velocidad: number) {
-    pins.analogWritePin(AnalogPin.P1, Math.round(Velocidad * 1023 / 100))
+    pins.analogWritePin(AnalogPin.P1, Math.round(Velocidad * 1023 / 10))
     pins.digitalWritePin(DigitalPin.P13, 0)
     pins.digitalWritePin(DigitalPin.P12, 1)
-    pins.analogWritePin(AnalogPin.P2, Math.round(Velocidad * 1023 / 100))
+    pins.analogWritePin(AnalogPin.P2, Math.round(Velocidad * 1023 / 10))
     pins.digitalWritePin(DigitalPin.P15, 1)
     pins.digitalWritePin(DigitalPin.P16, 0)
 }
-radio.onReceivedNumber(function (receivedNumber) {
-    basic.showNumber(receivedNumber)
-})
 function RotDer (Velocidad: number) {
-    pins.analogWritePin(AnalogPin.P1, Math.round(Velocidad * 1023 / 100))
+    pins.analogWritePin(AnalogPin.P1, Math.round(Velocidad * 1023 / 10))
     pins.digitalWritePin(DigitalPin.P13, 1)
     pins.digitalWritePin(DigitalPin.P12, 0)
-    pins.analogWritePin(AnalogPin.P2, Math.round(Velocidad * 1023 / 100))
+    pins.analogWritePin(AnalogPin.P2, Math.round(Velocidad * 1023 / 10))
     pins.digitalWritePin(DigitalPin.P15, 0)
     pins.digitalWritePin(DigitalPin.P16, 1)
 }
@@ -50,10 +47,10 @@ function Parar () {
     pins.digitalWritePin(DigitalPin.P16, 0)
 }
 function backward (vel: number) {
-    pins.analogWritePin(AnalogPin.P1, Math.round(vel * 1023 / 100))
+    pins.analogWritePin(AnalogPin.P1, Math.round(vel * 1023 / 10))
     pins.digitalWritePin(DigitalPin.P13, 0)
     pins.digitalWritePin(DigitalPin.P12, 1)
-    pins.analogWritePin(AnalogPin.P2, Math.round(vel * 1023 / 100))
+    pins.analogWritePin(AnalogPin.P2, Math.round(vel * 1023 / 10))
     pins.digitalWritePin(DigitalPin.P15, 0)
     pins.digitalWritePin(DigitalPin.P16, 1)
 }
@@ -66,20 +63,25 @@ function GiroDer (Velocidad: number) {
     pins.digitalWritePin(DigitalPin.P16, 1)
 }
 function forward (Velocidad: number) {
-    pins.analogWritePin(AnalogPin.P1, Math.round(Velocidad * 1023 / 100))
+    pins.analogWritePin(AnalogPin.P1, Velocidad)
     pins.digitalWritePin(DigitalPin.P13, 1)
     pins.digitalWritePin(DigitalPin.P12, 0)
-    pins.analogWritePin(AnalogPin.P2, Math.round(Velocidad * 1023 / 100))
+    pins.analogWritePin(AnalogPin.P2, Velocidad)
     pins.digitalWritePin(DigitalPin.P15, 1)
     pins.digitalWritePin(DigitalPin.P16, 0)
 }
 led.enable(false)
 pins.digitalWritePin(DigitalPin.P14, 1)
-radio.setGroup(1)
 basic.forever(function () {
-    for (let vel = 0; vel <= 100; vel++) {
-        forward(vel)
-        radio.sendNumber(vel)
-        basic.pause(5000)
+    if (pins.digitalReadPin(DigitalPin.P8) == 1 && pins.digitalReadPin(DigitalPin.P9) == 1) {
+        forward(1023)
+    } else if (pins.digitalReadPin(DigitalPin.P8) == 0 && pins.digitalReadPin(DigitalPin.P9) == 1) {
+        GiroIzq(1023)
+    } else if (pins.digitalReadPin(DigitalPin.P8) == 1 && pins.digitalReadPin(DigitalPin.P9) == 0) {
+        GiroDer(1023)
+    } else {
+        while (pins.digitalReadPin(DigitalPin.P8) == 1) {
+            GiroDer(1023)
+        }
     }
 })
